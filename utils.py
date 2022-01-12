c4a63ecca2ee974b2d6faaec4e6bafeaf7fead5d
Tikhon Zaikin <th@thevhs.club>
2022-01-12 13:17:48 +0100

    add utils

--- utils.py
+++ utils.py
@@ -19,6 +19,24 @@ def generate_declarations_in_module_from_implementations(module_directory=".", n
             f.write(str(declaration))
 
 
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import subprocess
+        import sys
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
 if __name__ == '__main__':
     module_name = "cara_sample_module"
+    # install_and_import(module_name)
+    cara_module_implementations = importlib.import_module(f"{module_name}.implementation")
+    node_type_declarations = [str(Node.get_declaration(node)) for node in cara_module_implementations]
+    node_instances = [node() for node in cara_module_implementations]
+
+
     generate_declarations_in_module_from_implementations(module_name)
